9dcd1f3715c6d9e2261ba0cea4ef45f1684ed2d4
stevenwoolston <steven.woolston@gmail.com>
2014-07-11 13:35:36 +1000

    Json helper class

---
+++ IssueTracker.Web/Helpers/JsonExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace IssueTracker.Web.Helpers
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson(this Object obj)
+        {
+            return new JavaScriptSerializer().Serialize(obj);
+        }
+    }
+}
